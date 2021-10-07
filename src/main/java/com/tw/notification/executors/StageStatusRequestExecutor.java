4d4337088f76fba2c264ce696965b1d242544cfa
Gobinath Manokaran <gobi12b@gmail.com>
2021-10-07 10:59:00 +0530

    Renamed the package to com.tw.notification from com.example.notification

---
+++ src/main/java/com/tw/notification/executors/StageStatusRequestExecutor.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2018 ThoughtWorks, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.tw.notification.executors;
+
+import com.tw.notification.PluginRequest;
+import com.tw.notification.RequestExecutor;
+import com.tw.notification.requests.StageStatusRequest;
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.thoughtworks.go.plugin.api.response.DefaultGoPluginApiResponse;
+import com.thoughtworks.go.plugin.api.response.GoPluginApiResponse;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class StageStatusRequestExecutor implements RequestExecutor {
+    private static final Gson GSON = new GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES).create();
+
+    private final StageStatusRequest request;
+    private final PluginRequest pluginRequest;
+
+    public StageStatusRequestExecutor(StageStatusRequest request, PluginRequest pluginRequest) {
+        this.request = request;
+        this.pluginRequest = pluginRequest;
+    }
+
+    @Override
+    public GoPluginApiResponse execute() throws Exception {
+        HashMap<String, Object> responseJson = new HashMap<>();
+        try {
+            sendNotification();
+            responseJson.put("status", "success");
+        } catch (Exception e) {
+            responseJson.put("status", "failure");
+            responseJson.put("messages", Arrays.asList(e.getMessage()));
+        }
+        return new DefaultGoPluginApiResponse(200, GSON.toJson(responseJson));
+    }
+
+    protected void sendNotification() throws Exception {
+        // TODO: Implement this. The request.pipeline object has all the details about the pipeline, materials, stages and jobs
+        // If you need access to settings like API keys, URLs, then call PluginRequest#getPluginSettings
+//        PluginSettings pluginSettings = pluginRequest.getPluginSettings();
+        System.out.println("FROM CHAT NOTIFICATION..");
+        throw new UnsupportedOperationException();
+    }
+}
